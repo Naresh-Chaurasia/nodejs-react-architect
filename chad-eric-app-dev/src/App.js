ad479e5b25225ccec884e1d4598cd1aeda97e099
Naresh Chaurasia <nareshchaurasia@Nareshs-MacBook-Pro.local>
2024-03-23 14:06:50 +0530

    Macbook Pro, Checkin Timestamp::Sat 23 Mar 2024 14:06:50 IST

--- chad-eric-app-dev/src/App.js
+++ chad-eric-app-dev/src/App.js
@@ -1,14 +1,9 @@
 import React, {useState} from 'react';
 import './App.css';
 import TodoTable from './components/TodoTable';
+import NewTodoForm from './components/NewTodoForm';
 
 function App() {
-
-  // const todos_list = [
-  //   { rowNumber: 1, rowDescription: 'Task1', rowAssigned: 'User1' },
-  //   { rowNumber: 2, rowDescription: 'Finish task 2', rowAssigned: 'User2' },
-  //   { rowNumber: 3, rowDescription: 'Submit task 3', rowAssigned: 'User3' },
-  // ];
 
   const [todos_list, setTodos_list] = useState([
       { rowNumber: 1, rowDescription: 'Task1', rowAssigned: 'User1' },
@@ -39,6 +34,7 @@ function App() {
       <button className='btn btn-primary' onClick={addToDo}>
             Add New Todo
       </button>
+      <NewTodoForm/>
     </div>
     </div>
     </div>
